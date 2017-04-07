7883cc107909949b88f28c674251a5c6b1139e97
Nabil Koroghli <nkoroghli@talentsoft.com>
2017-04-07 10:50:30 +0200

    v1

---
+++ app/webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/app'
+  },
+
+  plugins: [
+    new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: require('../dll/dist/lib-manifest.json')
+    })
+  ],
+  output: {
+        path: path.resolve(__dirname, "./dist/"),
+        publicPath: "../",
+        filename: "[name].bundle.[chunkhash].js"
+    }
+}
---
+++ dll/webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    lib: [ 'jquery' ]
+  },
+
+  plugins: [
+    new webpack.DllPlugin({
+      path: 'dist/[name]-manifest.json',
+      name: '[name]_lib'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: 'dist/',
+    library: '[name]_lib',
+  },
+}
